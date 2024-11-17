bf4eb82d0c8552bcecabfc77578b1611064643d7
@daynganxep <nguyenday3332@gmail.com>
2024-11-17 15:04:22 +0700

    task-fe-3

--- src/layouts/components/Header/SearchBox.jsx
+++ src/layouts/components/Header/SearchBox.jsx
@@ -1,19 +1,24 @@
+// src/components/SearchBox.jsx
 import React, { useState, useRef } from "react";
 import { FaSearch, FaTimes } from "react-icons/fa";
-import TollTip from "@components/Tooltip";
+import { useNavigate } from "react-router-dom"; 
 
 export default function SearchBox({ className }) {
   const [searchTerm, setSearchTerm] = useState("");
   const inputRef = useRef(null);
+  const navigate = useNavigate(); 
 
   const handleSearch = () => {
-    console.log("Tìm kiếm:", searchTerm);
+    if (searchTerm.trim()) {
+      navigate(`/search?q=${encodeURIComponent(searchTerm)}`);
+    }
   };
 
   const handleClear = () => {
     setSearchTerm("");
     inputRef.current.focus();
   };
+
   return (
     <div className={className + " flex-1 flex items-center mx-8"}>
       <div className="flex items-center w-full bg-gray-100 rounded-md p-1 pl-4">
@@ -21,25 +26,18 @@ export default function SearchBox({ className }) {
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="flex-1 bg-transparent focus:outline-none py-1" // Điều chỉnh chiều cao cho input
+          className="flex-1 bg-transparent focus:outline-none py-1"
           placeholder="Tìm kiếm sản phẩm..."
           ref={inputRef}
         />
         {searchTerm && (
-          <TollTip content="Xóa">
-            <button onClick={handleClear} className="text-500 p-2">
-              <FaTimes />
-            </button>
-          </TollTip>
-        )}
-        <TollTip content="Tìm kiếm">
-          <button
-            onClick={handleSearch}
-            className="bg-blue-500 text-white p-2 px-4 rounded-md hover:bg-blue-600" // Điều chỉnh padding
-          >
-            <FaSearch />
+          <button onClick={handleClear} className="text-gray-500 p-2">
+            <FaTimes />
           </button>
-        </TollTip>
+        )}
+        <button onClick={handleSearch} className="bg-blue-500 text-white p-2 px-4 rounded-md hover:bg-blue-600">
+          <FaSearch />
+        </button>
       </div>
     </div>
   );
